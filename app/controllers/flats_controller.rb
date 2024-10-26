52d5e0a958ffeb8942dedbce89c36ae023b3f9ea
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2024-10-26 17:10:05 +0200

    Etape 1 bis

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,23 +8,41 @@ class FlatsController < ApplicationController
   def show
     @flat = Flat.find(params[:id])
   end
-end
 
-def new
-  @flat = Flat.new
-end
-
-def create
-  @flat = Flat.new(flat_params)
-  if @flat.save
-    redirect_to @flat, notice: "Flat was successfully created."
-  else
-    render :new
+  # Affiche le formulaire pour un nouveau logement
+  def new
+    @flat = Flat.new
   end
-end
 
-private
+  # Crée un nouveau logement
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: "Flat was successfully created."
+    else
+      render :new
+    end
+  end
 
-def flat_params
-  params.require(:flat).permit(:name, :description, :price, :location)
+  # Affiche le formulaire d'édition pour un logement existant
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  # Met à jour un logement existant
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat), notice: "Flat updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  # Définit les paramètres autorisés pour un logement
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night)
+  end
 end
